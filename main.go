bbd97bab7359e2f4af0b47b18d092f01dd23dc9d
Iggy <ignatuszuk@gmail.com>
2023-02-15 14:13:12 +0100

    From StackOverflow.

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 	})
 
 	app.Use(func(c *fiber.Ctx) error {
-		c.Set("Content-Security-Policy", "default-src * 'unsafe-inline' 'unsafe-eval'; frame-src * 'unsafe-inline' 'unsafe-eval';")
+		c.Set("Content-Security-Policy", "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; font-src: * 'unsafe-inline';")
 		return c.Next()
 	})
 
